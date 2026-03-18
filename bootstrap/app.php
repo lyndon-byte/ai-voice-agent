ba186e8864592b20caee32df1a9f42441012bc12
donzkie <cuarterolyndon06@gmail.com>
2026-03-17 21:59:27 -0700

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -29,6 +29,7 @@ return Application::configure(basePath: dirname(__DIR__))
             // '/app/knowledge-base',
             '/receive-webhook'
         ]);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
